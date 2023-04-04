25ab4ee0736c52da843ca73cf26ea642a2fa0dfc
lenis03 <fardintorkamand83@gmail.com>
2023-04-04 22:21:42 +0430

    add test for HomePage and AboutUsPage

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'HomePage')
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class AboutUsTest(TestCase):
+    def test_aboutus_url_by_name(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_content(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertContains(response, 'AboutUs')
+
+    def test_aboutus_template_used(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertTemplateUsed(response, 'pages/aboutus.html')
+
