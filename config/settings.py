11c2b6c402a2f1e3cc6d65a37e0ff70da31ef5ee
lenis03 <fardintorkamand83@gmail.com>
2023-04-05 19:17:12 +0430

    add crispy forms for login and signup

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party
+    'crispy_forms',
+    'crispy_bootstrap4',
+
     'accounts',
     'pages'
 ]
@@ -135,5 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy forms config
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
