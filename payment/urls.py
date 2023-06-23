52b620fab2ba6b0ddc618035ef0b8462cbafd256
lenis03 <fardintorkamand83@gmail.com>
2023-06-23 15:29:09 +0330

    implement payment_callback and payment_process_sandbox & payment_callback_sandbox for test Payment gateway

--- payment/urls.py
+++ payment/urls.py
@@ -6,4 +6,6 @@ app_name = 'payment'
 
 urlpatterns = [
     path('process/', views.payment_process, name='payment_process'),
+    path('callback/', views.payment_callback, name='payment_callback'),
+
 ]
